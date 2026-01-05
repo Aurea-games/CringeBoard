defa0a5359dce3566ab3f7b39a790e9175065978
Volvie <baptiste.laran@epitech.eu>
2026-01-05 11:14:06 +0100

    Fix backend CI

--- backend/app/api/routes/aggregator/articles.py
+++ backend/app/api/routes/aggregator/articles.py
@@ -46,6 +46,7 @@ def list_popular_articles(
         order_by_popularity=True,
     )
 
+
 @router.get(
     "/favorite",
     response_model=list[schemas.Article],
@@ -54,6 +55,7 @@ def list_my_favorites(
     current_email: CurrentUserEmail,
 ) -> list[schemas.Article]:
     return aggregator_dependencies.aggregator_service.list_user_favorites(current_email)
+
 
 @router.get(
     "/{article_id}",
@@ -83,6 +85,7 @@ def favorite_article(
     current_email: CurrentUserEmail,
 ) -> schemas.Article:
     return aggregator_dependencies.aggregator_service.favorite_article(article_id, current_email)
+
 
 @router.delete(
     "/{article_id}/favorite",
